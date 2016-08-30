c6ac0ebe1a46cc058a77614a80cd2a65497e4e4c
Alec Larsen <alec.larsen@agapered.com>
2016-08-30 03:18:14 +0000

    Adds CodeClimate coverage reporter AND rubocop config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'codeclimate-test-reporter'
+
+CodeClimate::TestReporter.start
+
 require 'active_record'
 require 'active_record_distinct_on'
 
